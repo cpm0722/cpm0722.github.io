926e5074038f9d2b84aec0de532a7dcb027c063a
cpm0722 <cpm0722@gmail.com>
2021-01-19 16:31:06 +0900

    delete test post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,1 @@
-var store = [{
-        "title": "test title v1",
-        "excerpt":"\\[\\sum^N_{i=1}i\\] ","categories": [],
-        "tags": [],
-        "url": "http://0.0.0.0:4000/test/",
-        "teaser": null
-      }]
+var store = []
