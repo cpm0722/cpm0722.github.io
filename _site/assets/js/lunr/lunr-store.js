c11fc56c40e10d337a56384574966c0235db88a0
cpm0722 <cpm0722@gmail.com>
2021-01-19 15:10:58 +0900

    update test

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -71,6 +71,12 @@ var store = [{
         "url": "http://0.0.0.0:4000/XLNet-Generalized-Autoregressive-Pretraining-for-Language-Understanding/",
         "teaser": null
       },{
+        "title": "test",
+        "excerpt":"title: test title subtitle: test subtitle categories: test_category tags: test_tag date: 2021-01-19 14:33:00 +0000 use_math: true   \\[\\sum^N_{i=1}i\\] ","categories": [],
+        "tags": [],
+        "url": "http://0.0.0.0:4000/test/",
+        "teaser": null
+      },{
         "title": "Neural Machine Translation of Rare Words with Subword Units",
         "excerpt":"Archive Link: https://arxiv.org/abs/1508.07909 Created: Sep 21, 2020 3:12 PM Field: NLP Paper Link: https://arxiv.org/pdf/1508.07909.pdf Status: completed Submit Date: Aug 15, 2015 Backgrounds BLEU Score (Bilingual Evaluation Understudy) score \\[BLEU=min\\left(1,\\frac{\\text{output length}}{\\text{reference_length}}\\right)\\left(\\prod_{i=1}^4precision_i\\right)^{\\frac{1}{4}}\\] reference sentence와 output sentence의 일치율을 나타내는 score이다. 3단계 절차를 거쳐 최종 BLEU Score를 도출해낸다. n-gram에서 순서쌍의 겹치는 정도 (Precision) Example...","categories": ["Paper Review"],
         "tags": ["NLP"],
