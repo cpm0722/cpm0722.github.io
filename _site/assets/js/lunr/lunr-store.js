74f4af1f5c74c03007a4b9d644c1d6672400dfed
cpm0722 <cpm0722@gmail.com>
2021-01-19 15:40:38 +0900

    update v2

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -59,7 +59,7 @@ var store = [{
         "url": "http://0.0.0.0:4000/XLNet-Generalized-Autoregressive-Pretraining-for-Language-Understanding/",
         "teaser": null
       },{
-        "title": "test title v1",
+        "title": "test title v2",
         "excerpt":"\\[\\sum^N_{i=1}i\\] ","categories": [],
         "tags": [],
         "url": "http://0.0.0.0:4000/test/",
