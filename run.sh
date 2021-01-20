1b06dc6fac5c60f58bb8fdeebf88de022e5bd1a7
cpm0722 <cpm0722@gmail.com>
2021-01-20 18:15:05 +0900

    update about-me

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#docker run --volume="$PWD:/srv/jekyll" -p 4000:4000 -i -t jekyll/jekyll jekyll serve
+docker run --volume="$PWD:/srv/jekyll" -p 4000:4000 -i -t jekyll/jekyll jekyll serve
 
-docker run --volume="$PWD:/srv/jekyll" -p 4000:4000 -i -t jekyll/jekyll /bin/bash
+#docker run --volume="$PWD:/srv/jekyll" -p 4000:4000 -i -t jekyll/jekyll /bin/bash
